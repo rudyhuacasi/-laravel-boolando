11fba551a1bd3d6d1f18520ac7997964e67b4510
rudyhuacasi <alexisitaliano16@gmail.com>
2024-07-11 22:24:45 +0200

    {UPD} added footer.blade (php)

---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,20 @@
+<footer class="text-bg-dark">
+    <div class="container  py-3 px-2">
+        <div class="d-flex justify-content-between">
+            <div class="medium">
+                <strong>Booleando s.r.l.</strong><br>
+                <p class="top">Informazioni legali Inormativa sulla privacy Diritto di recesso</p>
+
+            </div>
+            <div class=" right-1 medium">
+                <p>Trovaci anche su</p>
+                <i class="fa-brands fa-square-twitter"></i>
+                <i class="fa-brands fa-square-facebook"></i>
+                <i class="fa-brands fa-square-instagram"></i>
+                <i class="fa-brands fa-square-pinterest"></i>
+                <i class="fa-brands fa-square-youtube"></i>
+
+            </div>
+        </div>
+    </div>
+</footer>
