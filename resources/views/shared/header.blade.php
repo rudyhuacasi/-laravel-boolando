c760ba2c80078d4e58e7e9ccaaceaf85bd07499f
rudyhuacasi <alexisitaliano16@gmail.com>
2024-07-11 22:23:42 +0200

    {UPD} added header.blade (php)

---
+++ resources/views/shared/header.blade.php
@@ -0,0 +1,25 @@
+<header>
+    <div class="container text-white">
+        <div class="d-flex justify-content-between py-4">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid">
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-link text-white" aria-current="page" href="#">Donna</a>
+                            <a class="nav-link text-white" href="#">Uomo</a>
+                            <a class="nav-link text-white" href="#">Bambini</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <div class="text-center">
+                <img class="w-50" src="{{ Vite::asset('resources/img/boolean-logo.png') }}" alt="paperella lavarel template">
+            </div>
+            <div class="icona d-flex align-items-center">
+                <i class="fa-regular fa-user"></i>
+                <i class="fa-regular fa-heart"></i>
+                <i class="fa-solid fa-bag-shopping"></i>
+            </div>
+        </div>
+    </div>
+</header>
